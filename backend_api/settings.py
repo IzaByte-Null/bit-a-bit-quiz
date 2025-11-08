a27e0525530192a2af08474da7763ca9d510ae10
iza072 <238135651+iza072@users.noreply.github.com>
2025-11-08 19:51:50 -0300

    Finaliza a configuração de segurança, usando variáveis de ambiente do Render

--- backend_api/settings.py
+++ backend_api/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(rnb*pi^t^jfms1k)nj8j$mb=+4p1_oo-f!y@lqy$4d4yyyp6b'
+# SECURITY WARNING: keep the secret key used in production secret! tirei por segurança (apenas no render)
+SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key_para_dev') # para o render
 
 
 # CONFIGURAÇÃO PARA DOIS AMBIENTES (LOCAL E DE PRODUÇÃO) 
@@ -36,8 +36,8 @@ if IS_PRODUCTION:
     DEBUG = False
     ALLOWED_HOSTS = ['iza72.pythonanywhere.com']
     
-    # CHAVE : PROVAVEL PROBLEMA NO PythonAnywhere
-    GEMINI_API_KEY = 'AIzaSyAWTerGbvZiWdqLdVdN8HwgZXxsXOZ-h9A' 
+    # CHAVE GIMINI
+    GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
     
     CORS_ALLOWED_ORIGINS = [
         "https://iza72.pythonanywhere.com",
