9a44e9952d3089ae7bf67db82933e6730af932f0
iza072 <238135651+iza072@users.noreply.github.com>
2025-11-10 20:17:50 -0300

    fix: URL API cadastro corrigida para /cadastro/ no RENDER

--- static/css/registro.js
+++ static/css/registro.js
@@ -1,4 +1,4 @@
-const API_CADASTRO_URL = 'https://bit-a-bit-quiz.onrender.com/cadastro/'; // <--- Corriginfo erro de cadastro no render 
+const API_CADASTRO_URL = '/cadastro/'; // <--- Corriginfo erro de cadastro no render 
 
 // tudo em DOMContentLoaded para garantir que os elementos existem na funçao/ usuarios
 document.addEventListener('DOMContentLoaded', function() {
